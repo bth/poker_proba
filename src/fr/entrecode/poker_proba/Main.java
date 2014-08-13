f415e7f4a99cef03099374d55886d692b33ec803
bth <contactbth@gmail.com>
2014-08-13 22:50:37 +0200

    Création de la classe Main.

---
+++ src/fr/entrecode/poker_proba/Main.java
@@ -0,0 +1,27 @@
+package fr.entrecode.poker_proba;
+
+public class Main {
+	
+	private Carte carteA;
+	private Carte carteB;
+	private float probabilite;
+	public Carte getCarteA() {
+		return carteA;
+	}
+	public void setCarteA(Carte carteA) {
+		this.carteA = carteA;
+	}
+	public Carte getCarteB() {
+		return carteB;
+	}
+	public void setCarteB(Carte carteB) {
+		this.carteB = carteB;
+	}
+	public float getProbabilite() {
+		return probabilite;
+	}
+	public void setProbabilite(float probabilite) {
+		this.probabilite = probabilite;
+	}
+
+}
