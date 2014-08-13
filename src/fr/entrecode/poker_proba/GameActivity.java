6c0effa63b680903120c2da0e4f86ddbcd9b9092
bth <contactbth@gmail.com>
2014-08-13 22:42:12 +0200

    Suppression des import inutiles.

--- src/fr/entrecode/poker_proba/GameActivity.java
+++ src/fr/entrecode/poker_proba/GameActivity.java
@@ -1,23 +1,6 @@
 package fr.entrecode.poker_proba;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.content.res.Resources;
-import android.graphics.Color;
-import android.graphics.Typeface;
 
 
 public class GameActivity extends Activity {
